df201db47b5697124005e143b3a6f9f486244cf2
Deven S <shriramsoft@gmail.com>
2025-04-02 11:55:32 +0530

    upgrade , logo

---
+++ src/types/alpine.d.ts
@@ -0,0 +1,9 @@
+declare module 'alpinejs' {
+    const Alpine: any;
+    export default Alpine;
+}
+
+declare module '@alpinejs/collapse' {
+    const collapse: any;
+    export default collapse;
+} 
